dbb739b2d1f848786cfe0354311e147d24bbb2c6
Robert Garcia <RGarcia35@mail.sfsu.edu>
2022-03-18 13:16:31 -0700

    runServer.JS now working correctly, need to modify VPTestHome so that results show on page

--- application/runServer.js
+++ application/runServer.js
@@ -3,12 +3,31 @@ const app = express()
 const mysql = require('mysql');
 let path = require('path');
 
+app.engine('html', require('ejs').renderFile);
+
+app.get('/VPTestHome.html', search, (req, res) => {
+    var searchResult = req.searchResult;
+    for (var i = 0; i < searchResult.length; i++){
+        console.log(searchResult[i]['Name']);
+    }
+    res.render('html/VPTestHome.html', {
+        
+        results: searchResult.length,
+        searchTerm: req.searchTerm,
+        searchResult: searchResult,
+        category: req.category
+    });
+})
 
 app.use(express.static('public/html'));
 app.use(express.static('public/html/aboutPages'));
 app.use(express.static('public/images'));
 app.use(express.static('public/css'));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.listen(3000, () => console.log('Server running on port 3000'));
 
 
 const database = mysql.createConnection({
@@ -26,7 +45,7 @@ database.connect((err) => {
 
 database.query('SELECT * FROM Posting', (error, results, fields) =>{
     if (error) throw error;
-    console.log(results);
+    //console.log(results);
 });
 
 function search(req, res, next) {
@@ -35,10 +54,10 @@ function search(req, res, next) {
     let query = 'SELECT * FROM Posting'
 
     if (searchTerm != '' && category != ''){
-        query = `SELECT * FROM Posting WHERE Category = '` + category + `' AND ( Name LIKE '%` + searchTerm + `%' OR Comment LIKE '%` + searchTerm + `%')`;
+        query = `SELECT * FROM Posting WHERE Category = '` + category + `' AND ( Name LIKE '%` + searchTerm + `%' OR Comment LIKE '%` + searchTerm + `%' OR Category LIKE '%` + searchTerm + `%')`;
     }
     else if (searchTerm != '' && category == ''){
-        query = `SELECT * FROM Posting WHERE Name LIKE '%` + searchTerm + `%' OR Comment LIKE '%` + searchTerm + `%'`;
+        query = `SELECT * FROM Posting WHERE Name LIKE '%` + searchTerm + `%' OR Comment LIKE '%` + searchTerm + `%' OR Category LIKE '%` + searchTerm + `%'`;
     }
     else if (searchTerm == '' && category != ''){
         query = `SELECT * FROM Posting WHERE Category = '` + category + `'`;
@@ -61,15 +80,6 @@ function search(req, res, next) {
 }
 
 
-app.get('/testSearch', search, (req, res) => {
-    console.log("HI");
-    var searchResult = req.searchResult
-    res.render('html/testSearch', {
-        results: searchResult.length,
-        searchTerm: req.searchTerm,
-        searchResult: searchResult,
-        category: req.category
-    });
-})
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+
