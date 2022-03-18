f12cdba1ae837c50b0385dcb79f3711474c97f73
Robert Garcia <RGarcia35@mail.sfsu.edu>
2022-03-18 13:22:23 -0700

    Adjusted SQL file

--- application/Example.sql
+++ application/Example.sql
@@ -3,4 +3,26 @@ CREATE DATABASE Example;
 USE Example;
 DROP TABLE IF EXISTS Posting;
 
-SELECT * FROM example.posting;
+CREATE TABLE Posting (
+ID int,
+Name varchar(255),
+Category varchar(16),
+UserID int,
+Comment varchar(255),
+Price double
+);
+
+INSERT INTO posting (ID, Name, Category, UserID, Comment, Price)
+VALUES
+(1, 'Learning Life: The Path to Academic Success and Personal Happiness', 'Textbook', 1, 'Textbook for Self Improvement class, it`s just like new!', 20),
+(2, 'Calculus: Early Transcendentals', 'Textbook', 2, 'Textbook for Calculus, it is slightly used..', 12),
+(3, 'Physics for Scientists and Engineers: A Strategic Approach', 'Textbook', 3, 'Textbook for physics, it is used/damaged', 5),
+(4, 'Understanding and Using English Grammar', 'Textbook', 4, 'Book for english class, good Condition, can have for free', 0),
+(5, 'CSC 648 Notes', 'Notes', 5, 'Notes for Software Engineering, incredibly indepth', 100),
+(6, 'CSC 665 Notes', 'Notes', 6, 'Notes for Artifical Intelligence, not much written', 5),
+(7, 'CSC 642 Notes', 'Notes', 7, 'Notes for Human Computer Interaction, simple', 3),
+(8, 'Coffee Table', 'Furniture', 8, 'Slightly stained coffee table, no damage, need gone ASAP!', 7),
+(9, 'Computer Chair', 'Furniture', 9, 'Old computer chair; crusty, but comfortable!', 15),
+(10, 'Couch', 'Furniture', 10, 'Freshly bought couch, doesnt fit in my dorm!', 30);
+
+SELECT * FROM Example.Posting;
