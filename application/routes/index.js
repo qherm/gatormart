5bf112f63d5094ff30c429463f9a57e907a47e44
cctina516 <christinayan1019@gmail.com>
2022-04-28 19:23:27 -0700

    Add total received messages, Update price input length, Update location selection, Update button space, Add route

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,71 +1,78 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 // var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 // var { getRecentPosts, getPostById, getCommentsByPostId } = require('../middleware/postsmiddleware');
 // var db = require('../config/database');
 
-
-router.get('/', function (req, res, next) {
-  res.render('home');
+router.get("/", function (req, res, next) {
+  res.render("home");
 });
 
-router.get('/home', function (req, res, next) {
-  res.render('home');
+router.get("/home", function (req, res, next) {
+  res.render("home");
 });
 
-router.get('/aboutUs', function (req, res, next) {
-  res.render('aboutUs');
+router.get("/aboutUs", function (req, res, next) {
+  res.render("aboutUs");
 });
 
-router.get('/aboutMelissa', function (req, res, next) {
-  res.render('aboutMelissa');
+router.get("/aboutMelissa", function (req, res, next) {
+  res.render("aboutMelissa");
 });
 
-router.get('/aboutShane', function (req, res, next) {
-  res.render('aboutShane');
+router.get("/aboutShane", function (req, res, next) {
+  res.render("aboutShane");
 });
 
-router.get('/aboutMinggu', function (req, res, next) {
-  res.render('aboutMinggu');
+router.get("/aboutMinggu", function (req, res, next) {
+  res.render("aboutMinggu");
 });
 
-router.get('/aboutChuting', function (req, res, next) {
-  res.render('aboutChuting');
+router.get("/aboutChuting", function (req, res, next) {
+  res.render("aboutChuting");
 });
 
-router.get('/aboutRobert', function (req, res, next) {
-  res.render('aboutRobert');
+router.get("/aboutRobert", function (req, res, next) {
+  res.render("aboutRobert");
 });
 
-router.get('/aboutJoe', function (req, res, next) {
-  res.render('aboutJoe');
+router.get("/aboutJoe", function (req, res, next) {
+  res.render("aboutJoe");
 });
 
-router.get('/aboutXiaoqing', function (req, res, next) {
-  res.render('aboutXiaoqing');
+router.get("/aboutXiaoqing", function (req, res, next) {
+  res.render("aboutXiaoqing");
 });
 
-router.get('/login', (req, res, next) => {
+router.get("/login", (req, res, next) => {
   res.render("login");
-})
+});
 
-router.get('/registration', (req, res, next) => {
+router.get("/registration", (req, res, next) => {
   res.render("registration");
-})
+});
 
-router.get('/productDetail', (req, res, next) => {
-    res.render("productDetail");
-  })
+router.get("/productDetail", (req, res, next) => {
+  res.render("productDetail");
+});
+
+router.get("/message", (req, res, next) => {
+  res.render("message");
+});
+
+router.get("/searchResult", (req, res, next) => {
+  res.render("searchResult");
+});
 
 // router.use("/postItem", isLoggedIn);
-router.get('/postItem', (req, res, next) => {
+router.get("/postItem", (req, res, next) => {
   res.render("postItem");
-})
+});
 
 // router.use("/userPage", isLoggedIn);
-router.get('/userPage', (req, res, next) => {
+router.get("/userPage", (req, res, next) => {
   res.render("userPage");
-})
+});
 
 // router.get('/posts/:id(\\d+)', (req, res, next) => {
 //   res.render('imagepost', { title: `Post ${req.params.id}` });
--- application/runServer.js
+++ application/runServer.js
@@ -1,51 +1,49 @@
-const express = require('express')
-var app = express()
-let path = require('path');
-var handlebars = require('express-handlebars');
-var indexRouter = require('./routes/index');
+const express = require("express");
+var app = express();
+let path = require("path");
+var handlebars = require("express-handlebars");
+var indexRouter = require("./routes/index");
 // var usersRouter = require('./routes/users');
 // var postRouter = require('./routes/posts');
 // var commentRouter = require('./routes/comments');
 
 app.engine(
-    "hbs",
-    handlebars({
-        layoutsDir: path.join(__dirname, "views/layouts"),
-        partialsDir: path.join(__dirname, "views/partials"),
-        extname: ".hbs",
-        defaultLayout: "body",
-        helpers: {
-            emptyObject: (obj) => {
-                return !(obj.constructor === Object && Object.keys(obj).length == 0);
-            }
-        }
-    })
+  "hbs",
+  handlebars({
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+    extname: ".hbs",
+    defaultLayout: "body",
+    helpers: {
+      emptyObject: (obj) => {
+        return !(obj.constructor === Object && Object.keys(obj).length == 0);
+      },
+    },
+  })
 );
-
 
 app.set("view engine", "hbs");
 // app.use(express.static('public/html'))
 // app.use(express.static('public/html/aboutPages'))
-app.use(express.static('public/images'))
-app.use(express.static('public/css'))
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
+app.use(express.static("public/images"));
+app.use(express.static("public/css"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
 // app.use('/users', usersRouter);
 // app.use('/posts', postRouter);
 // app.use('/comments', commentRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.render('error', { err_message: err });
-})
+  console.log(err);
+  res.render("error", { err_message: err });
+});
 
 // app.use((req, res, next) => {
 //     requestPrint(req.url);
 //     next();
 // });
-
 
 module.exports = app;
