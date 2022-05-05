377ce7cda6fc02d54f36ba5526320119bb1d6c12
Robert Garcia <RGarcia35@mail.sfsu.edu>
2022-05-04 20:05:55 -0700

    Blueprint for the backend code in terms of Registration is done. Still need to edit SQL files and work on further functions

--- application/runServer.js
+++ application/runServer.js
@@ -17,8 +17,6 @@ app.get('/*/:search/:category', search, (req, res) => {
 
 })
 
-
-
 app.use(express.static('public/html'));
 app.use(express.static('public/html/aboutPages'));
 app.use(express.static('public/images'));
@@ -72,10 +70,11 @@ function search(req, res, next) {
         }
         req.searchResult = result;
         req.searchTerm = searchTerm;
-        req.category = "";
+        req.category = category;
         next();
     });
 }
+
 
 function Register(req, res, next) {
     var userEmail = req.params.userEmail;
@@ -84,23 +83,77 @@ function Register(req, res, next) {
     var userConfirmPassword = req.params.userConfirmPassword;
     //if validate == false, don't register
 
-    if (ValidateUserExists){
+    if (ValidateUserExists(userEmail)){
+        req.resultMessage = "Failure";
         return -1
     }
-    else if( !isValidPassword){
+    else if( !isValidPassword(userPassword, userConfirmPassword)){
+        req.resultMessage = "Failure";
         return -1
     }
-    else if (!isValidEmail){
+    else if (!isValidEmail(userEmail)){
+        req.resultMessage = "Failure";
         return -1
     }
     
+    userEncryptedPassword = encryptPassword(userPassword);
     //add user to DB
+
+    let query = `INSERT INTO User (Name, Email, Password) VALUES ('` 
+                    + userName +  `', '`
+                    + userEmail +  `', '`
+                    + userEncryptedPassword +  `')`;
+
+
+    database.query(query, (err, result) => {
+        if (err){
+            req.resultMessage = "Failure";
+
+            next();
+        }
+        req.resultMessage = "Success";
+        next();
+    });    
+    
 }
 
+function Login(req, res, next) {
+}
 
-function ValidateUserExists() {
+function Post(req, res, next) {
+}
+
+function Message(req, res, next) {
+}
+
+function ValidateUserExists(userEmail) {
     //check if user is already in DB, if so, return true
-    let query = 'SELECT * FROM User'
+    let query = `SELECT * FROM User WHERE Email = '` + userEmail + `'` 
+    database.query(query, (err, result) => {
+        
+        if (err){
+            req.userExistsMessage = "Failure to check";
+            return -1;
+            //next();
+        }
+        //if the user does not exist
+        if(result.length === 0){
+            req.userExistsMessage = "User does not exist in DB";
+            return 1;
+        }
+        else{
+            req.userExistsMessage = "User does exist in DB";
+            return -1;
+        }
+    });  
+}
+
+function encryptPassword(password){
+
+}
+
+function decryptPassword(password){
+
 }
 
 function isValidPassword(password, confirmpassword) {
@@ -115,5 +168,9 @@ function isValidPassword(password, confirmpassword) {
 }
 
 function isValidEmail(email){
-    return true;
+    let userEmail = email.toLowerCase();
+    if (/@mail.sfsu.edu\s*$/.test(userEmail)) {
+        return true;
+    } 
+    return false;
 }
