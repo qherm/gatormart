ecf6f7a1d2f1d6b18755c55b871002c34c9f6b1b
Robert Garcia <RGarcia35@mail.sfsu.edu>
2022-05-09 20:58:32 -0700

    Tons of changes made, a lot of it to do with the structure of runServer.js

--- application/runServer.js
+++ application/runServer.js
@@ -1,4 +1,3 @@
-const e = require('express');
 const express = require('express');
 const { connect } = require('http2');
 const app = express()
@@ -6,19 +5,6 @@ const mysql = require('mysql');
 let path = require('path');
 
 app.engine('html', require('ejs').renderFile);
-
-app.get('/*/:search/:category', search, (req, res) => {
-    var searchResult = req.searchResult;
-    res.json({
-        results: searchResult.length,
-        searchTerm: req.searchTerm,
-        searchResult: searchResult,
-        category: req.category
-    });
-
-})
-
-
 
 app.use(express.static('public/html'));
 app.use(express.static('public/html/aboutPages'));
@@ -29,7 +15,6 @@ app.use(express.static('public/css'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.listen(3000, () => console.log('Server running on port 3000'));
-
 
 const database = mysql.createConnection({
     host: 'localhost',
@@ -47,7 +32,15 @@ database.connect((err) => {
     
 });
 
-
+app.get('/*/search/:search/:category', search, (req, res) => {
+    var searchResult = req.searchResult;
+    res.json({
+        results: searchResult.length,
+        searchTerm: req.searchTerm,
+        searchResult: searchResult,
+        category: req.category
+    });
+});
 
 function search(req, res, next) {
     var searchTerm = req.params.search;
@@ -82,12 +75,55 @@ function search(req, res, next) {
 }
 
 //userFullName, userEmail, username, userPassword, userConfirmPassword
-app.post('/*/:userFullName/:userEmail/:username/:userPassword/:userConfirmPassword', Register, (req, res) => {
-    res.json({
-        finalMessage: req.resultMessage,
-    });
-    
-})
+app.post('/register', Register.register);
+
+class Auth {
+    emailExists(email,callback) {
+        database.query("SELECT * FROM user WHERE email=?",email,(err,results)=> {
+            if(err) throw err;
+            return callback(results);
+        })
+    }
+
+    usernameExists(username,callback) {
+        database.query("SELECT * FROM user WHERE email=? OR Username=?",username,(err,results)=> {
+            if(err) throw err;
+            return callback(results);
+        })
+    }
+
+    encryptPassword(password){
+        
+    }
+}
+
+class Register extends Auth {
+    register(req, res, next){
+        console.log("IN POST: ",req.resultMessage);
+        let email = req.body.email;
+        let name = req.body.name;
+        let username = req.body.username;
+        let password = req.body.password;
+        let confirmPassword = req.body.confirmPassword;
+
+        res.json({
+            finalMessage: req.resultMessage,
+        });
+    }
+}
+
+class Login extends Auth {
+    login(req,res,next){
+
+    }
+}
+
+let emailExists = (email, callback) => {
+    database.query("SELECT * FROM user WHERE email=?",[email],(err,results)=> {
+        if(err) throw err;
+        return callback(results);
+    })
+}
 
 function Register(req, res, next) {
     req.valid = false;
@@ -98,50 +134,86 @@ function Register(req, res, next) {
     var userName = req.params.username;
     var userPassword = req.params.userPassword;
     var userConfirmPassword = req.params.userConfirmPassword;
-    //if validate == false, don't register
+
     
-    if (ValidateUserExists(userEmail)){
-        req.resultMessage += "User exists in System already. ";
-        return false;
-    }
-
-
-    if( !isValidPassword(userPassword, userConfirmPassword)){
-        req.resultMessage += "User Password not valid. ";
-        return false;
-    }
-
-
-    if (!isValidEmail(userEmail)){
-        req.resultMessage += "User email not valid. ";
-        return false;
-    }
-
-    //${userFullName}/${userEmail}/${username}/${userPassword}/${userConfirmPassword}
-
-    userEncryptedPassword = userPassword;
-    //userEncryptedPassword = encryptPassword(userPassword);
-    //add user to DB
-    let query = `INSERT INTO User (name, Email, Username, Password, Bio_Description, Phone_Number) VALUES ('` 
-                    + userFullName +  `', '`
-                    + userEmail +  `', '`
-                    + userName +  `', '`
-                    + userEncryptedPassword +  `', '`
-                    + '' +  `', '`
-                    + '' +  `')`;
-
-
-    database.query(query, (err, result) => {
-        if (err){
-            console.log(err);
-            req.resultMessage += "Failure, error in DB";
-            req.valid = false;
+    emailExists(userEmail, (results) =>{
+        console.log("results: ", results);
+        if(results.length>0){ 
+            req.resultMessage = "User already exists. This is a test string";
             next();
+        } else{
+
         }
-        req.resultMessage += "success";
-        req.valid = true;
-        next();
-    });    
+    });
+    // console.log("THIS IS OUR TEST: ",test);
+    /*
+    CREATE TABLE IF NOT EXISTS `GatorMartDB`.`User` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(225) NOT NULL,
+    `Email` VARCHAR(225) UNIQUE NOT NULL,
+    `Username` VARCHAR(225) UNIQUE NOT NULL,
+    `Password` VARCHAR(225) NOT NULL,
+    `Bio_Description` VARCHAR(225) NOT NULL,
+    `Phone_Number` VARCHAR(225) NOT NULL,
+    PRIMARY KEY (`ID`));
+    */
+
+    // database.query("SELECT * FROM User WHERE email=?",[userEmail], (err, rows, f) => {
+    //     if(err) throw err
+    //     if(rows.length>0){
+    //         console.log("I'm in here");
+    //         req.resultMessage = "User with that email already exists";
+    //         console.log(req.resultMessage);
+    //     }
+    // });
+    
+    // console.log(req.resultMessage);
+    // if(req.resultMessage.length > 0) return;
+    // console.log("I'm down here");
+
+    // //if validate == false, don't register
+    // console.log("\nDOES VALID USER EXIST????", ValidateUserExists(userEmail),"\n");
+    
+    // //if (ValidateUserExists(userEmail)){
+    // //    req.resultMessage += "User exists in System already. ";
+    // //    return false;
+    // //}
+
+    // if( !isValidPassword(userPassword, userConfirmPassword)){
+    //     req.resultMessage += "User Password not valid. ";
+    //     return false;
+    // }
+
+    // if (!isValidEmail(userEmail)){
+    //     req.resultMessage += "User email not valid. ";
+    //     return false;
+    // }
+
+    // //${userFullName}/${userEmail}/${username}/${userPassword}/${userConfirmPassword}
+
+    // userEncryptedPassword = userPassword;
+    // //userEncryptedPassword = encryptPassword(userPassword);
+    // //add user to DB
+    // let query = `INSERT INTO User (name, Email, Username, Password, Bio_Description, Phone_Number) VALUES ('` 
+    //                 + userFullName +  `', '`
+    //                 + userEmail +  `', '`
+    //                 + userName +  `', '`
+    //                 + userEncryptedPassword +  `', '`
+    //                 + '' +  `', '`
+    //                 + '' +  `')`;
+
+
+    // database.query(query, (err, result) => {
+    //     if (err){
+    //         console.log(err);
+    //         req.resultMessage += userEmail + "Failure, error in DB";
+    //         req.valid = false;
+    //         next();
+    //     }
+    //     req.resultMessage += "success";
+    //     req.valid = true;
+    //     next();
+    // });
     
 }
 
@@ -157,36 +229,20 @@ function Message(req, res, next) {
 //doesn't seem to work with async
 function ValidateUserExists(userEmail) {
     //check if user is already in DB, if so, return true
-    let query = `SELECT * FROM User WHERE Email = '` + userEmail + `'` 
-    //return await database.query(query, (err, result) => {
-    database.query(query, (err, result) => {    
-        if (err){
-            //req.userExistsMessage = ;
-            console.log("Failure to check");
-            return true;
-            //next();
-        }
-        //if the user does not exist
-        if(result.length === 0){
-            //req.userExistsMessage = ;
-            console.log("User does not exist in DB");
-            return false;
-        }
-        else{
-            //req.userExistsMessage = ;
-            console.log("User does exist in DB");
-            return true;
-        }
-    });  
+    
+    let query = `SELECT * FROM User WHERE Email = '` + userEmail + `'`;
+    database.query(query, async (err, result) => {
+        console.log(await result);
+        setXToV(await result);
+        console.log("\nTEST x is:", x);
+    });
+    console.log("\nx is:", x);
 }
 
 function encryptPassword(password){
 
 }
 
-function decryptPassword(password){
-
-}
 
 function isValidPassword(password, confirmpassword) {
     // console.log(password);
