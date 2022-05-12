2c353abc04dce9bdae667edd83fa0e1589519ed9
Robert Garcia <RGarcia35@mail.sfsu.edu>
2022-05-11 20:19:30 -0700

    Beginning to implement the code that allows the results of a search to be displayed on the search results page

--- application/routes/search.js
+++ application/routes/search.js
@@ -14,17 +14,17 @@ class Search {
         let category = req.query.category;
         let query;
         console.log(search, category);
-        
-        if(!category.length&&!search.length){
+
+        if (!category.length && !search.length) {
             query = "SELECT * FROM posts";
-        } else if(!category.length){
+        } else if (!category.length) {
             query = `SELECT * FROM posts WHERE title LIKE '%` + search + `%' OR description LIKE '%` + search + `%' OR category LIKE '%` + search + `%'`;
-        } else if(!search.length){
+        } else if (!search.length) {
             query = `SELECT * FROM posts WHERE category = '` + category + `'`;
-        } else{
+        } else {
             query = `SELECT * FROM posts WHERE category = '` + category + `' AND ( Title LIKE '%` + search + `%' OR description LIKE '%` + search + `%' OR Category LIKE '%` + search + `%')`;
         }
-        
+
         // if (searchTerm != 'EMPTYSEARCHTEMP' && category != 'EMPTYCATEGORYTEMP'){
         //     query = `SELECT * FROM Post WHERE Category = '` + category + `' AND ( Title LIKE '%` + searchTerm + `%' OR Post_Description LIKE '%` + searchTerm + `%' OR Category LIKE '%` + searchTerm + `%')`;
         // }
@@ -40,7 +40,7 @@ class Search {
         console.log(query);
         database.query(query, (err, result) => {
             console.log("in db query")
-            if (err){
+            if (err) {
                 res.render('searchResult', {
                     result: "",
                     category: category,
@@ -57,6 +57,38 @@ class Search {
             }
         });
     }
+
+    displayResults(req, res) {
+        var searchResults = "";
+        var searchTerm = "";
+        var category = "";
+
+        for (let i = 0; i < searchResults.length; i++) {
+            document.getElementById("searchResults").innerHTML +=
+            `
+                <div class="col-md-3 mb-2">
+                    <div class="card shadow" style="width: 18rem;">
+                        <a href="#">
+                            <img
+                                src=""
+                                class="card-img-top"
+                                alt="..."
+                            />
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title font-poppins">searchResults[i].title</h5>
+                                <span>textbooks </span>
+                            <h6 class="mb-3">searchResults[i].price</h6>
+                            <a href="productDetail" class="btn btn-primary font-size-09 text-light product-button">View Details</a>
+                            <a href="message" class="btn btn-primary font-size-09 text-light product-button">Message Seller</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+
+
 }
 
 let search = new Search();
