b045bb120f7ad386c68fc3c5f083053bad142fc3
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-13 17:23:32 -0700

    almost finished with upload file logic.

--- application/routes/item.js
+++ application/routes/item.js
@@ -1,8 +1,8 @@
 const database = require('../db/db.js');
 const express = require('express');
 const router = express.Router();
-let bodyParser = require('body-parser');
-// let jsonParser = bodyParser.json();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 /* 
     Methodology:
@@ -22,6 +22,7 @@ class Post {
     postItem(req, res){
         // Get params
         console.log(req.body);
+        console.log(req.file)
         let query = "";
         database.query(query, (err, result) => {
             if(err){
@@ -35,11 +36,24 @@ class Post {
 
 const post = new Post();
 
+router.use(fileUpload({
+    limits: {fileSize: 20000000} // 20 MB file upload limit
+}))
+
 router.get("/", post.renderItemPage);
 
 router.get('/post', (req, res) => {
     res.render('post');
 });
-router.post('/post', bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}), post.postItem);
+
+router.post('/grog', (req,res)=> {
+    console.log(req.files)
+    res.send("temp")
+});
+
+router.post('/post',fileUpload({
+    limits: {fileSize: 20000000} // 20 MB file upload limit
+}),  bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}), post.postItem);
+// router.post('/post', bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}), post.postItem);
 
 module.exports = router;
