9b58deff073cc51272e731a81c6fa24b95ab76bc
ShaneWaxler1 <swaxler@mail.sfsu.edu>
2022-05-16 21:59:53 -0700

    fixed user page rendering

--- application/routes/user.js
+++ application/routes/user.js
@@ -5,38 +5,62 @@ const database = require('../db/db.js');
 const sessions = require('../sessions');
 
 class UserDetails {
-    findUser(req, res) {
-        let userId = req.query.id;
-        let query = "";
-        
-        if(userId==="null" || parseInt(userId)==req.session.user_id){
-            userId = req.session.user_id;
-        }
-
-        query = 
-            `SELECT users.full_name, users.email, users.username, users.bio, posts.id, posts.title, posts.category, posts.description, posts.price, images.image_link
-            FROM users 
-            JOIN posts
-            ON posts.user_id = users.id 
-            JOIN images
-            ON images.post_id = posts.id
-            WHERE users.id = '` + userId + `'`;
-
-        database.query(query, (err, results, next) => {
+    getPostsAndImagesFromUserId(req,res,next){
+        let query =
+        `SELECT posts.id, posts.user_id, posts.title, posts.category, posts.description, posts.price, images.image_link
+        FROM posts
+        JOIN images
+        ON images.post_id = posts.id
+        WHERE posts.user_id = '` + req.userId + `'`;;
+        database.query(query, (err, results) => {
             if (err){
-                res.json({});
+                throw err;
             } else{
-                res.json({
-                    userInfo:results
-                });
+                console.log(results);
+                res.locals.posts = results;
+                next();
             }
         });
     }
+
+    findUser(req, res, next) {
+        const query = 
+            `SELECT full_name, email, username, bio FROM users WHERE id = '` + req.userId + `'`;
+
+        database.query(query, (err, results) => {
+            if (err){
+                throw err;
+            } else{
+                console.log(results)
+                res.locals.full_name = results[0].full_name;
+                res.locals.email = results[0].email;
+                res.locals.username = results[0].username;
+                res.locals.bio = results[0].bio;
+                next();
+            }
+        });
+    }
+
+    userExists(req,res,next){
+        if(req.query.id == 0){
+            res.redirect("/");
+            return;
+        } else if(!req.query.id && !req.session.user_id){
+            req.session.last_visited = "/user" + req.url.substring(1);
+            res.redirect("/auth/login");
+            return;
+        } else if(!req.query.id && req.session.user_id){
+            req.userId = req.session.user_id;
+        } else{
+            req.userId = req.query.id;
+        }
+        next();
+    }
 }
 
-const User = new UserDetails();
+const user = new UserDetails();
 
-router.get('/', (req,res)=>{
+router.get('/', user.userExists, user.findUser, user.getPostsAndImagesFromUserId, (req,res,next)=>{
     if(req.session.user_id){
         res.render('user');
     } else{
@@ -44,6 +68,6 @@ router.get('/', (req,res)=>{
         res.redirect("/auth/login");
     }
 });
-router.get('/json', User.findUser);
+// router.get('/json', User.findUser);
 
 module.exports = router;
